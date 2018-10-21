78bd474acdb75bb3a00d9d02d6ae70a8d1aace48
Tim <timothyolaleke@gmail.com>
2018-10-21 12:17:18 +0100

    Updates

---
+++ precache-manifest.3b1436f8ee60a8b4b4d6e51499a7667f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d44cad25b479954309cc81d0cf61b24",
+    "url": "/static/media/logo.2d44cad2.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "806477bfc8505f318516",
+    "url": "/static/js/main.806477bf.chunk.js"
+  },
+  {
+    "revision": "0690a69093681e38b854",
+    "url": "/static/js/1.0690a690.chunk.js"
+  },
+  {
+    "revision": "806477bfc8505f318516",
+    "url": "/static/css/main.5ae20e66.chunk.css"
+  },
+  {
+    "revision": "0690a69093681e38b854",
+    "url": "/static/css/1.7c70fd75.chunk.css"
+  },
+  {
+    "revision": "8d64c2643e953442d8f352550e1957ff",
+    "url": "/index.html"
+  }
+];
