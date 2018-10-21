e313739f6c04f4e4ed69f9417d5adf2319a19c51
Tim <timothyolaleke@gmail.com>
2018-10-21 11:56:25 +0100

    Added CouchDb Implementation

--- src/lib/userServices.js
+++ src/lib/userServices.js
@@ -1,14 +1,18 @@
 import PouchDB from 'pouchdb'
+import remoteDB from './remoteDB'
+
 
 const db = new PouchDB('github-profiles')
+let remoteDb = new PouchDB(remoteDB)
+
 export const fetchUser = async(userID) => {
     try {
         const req = await fetch(`https://api.github.com/users/${userID}`)
         const user = await req.json()
-        if(!user.login){
+        if (!user.login) {
             throw new Error()
         }
-        const profile = {_id:''+user.id,avatar_url:user.avatar_url,name:user.name,html_url:user.html_url,login:user.login}
+        const profile = { _id: '' + user.id, avatar_url: user.avatar_url, name: user.name, html_url: user.html_url, login: user.login }
         await db.put(profile)
         return profile
     } catch (err) {
@@ -16,10 +20,19 @@ export const fetchUser = async(userID) => {
     }
 }
 
-export const getAllUsers = async()=>{
+const syncDb = () => {
+    db.sync(remoteDb).on('complete', function() {
+        console.log("Sync success")
+    }).on('error', function(err) {
+        console.log("Error")
+    });
+}
+
+export const getAllUsers = async() => {
     try {
-        const req = await db.allDocs({include_docs:true})
-        const users =req.rows.map(row=>row.doc);
+        const req = await db.allDocs({ include_docs: true })
+        const users = req.rows.map(row => row.doc);
+        syncDb()
         return users
     } catch (err) {
         return {}
