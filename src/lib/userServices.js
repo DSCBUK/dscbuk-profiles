bf6c7caecc20cb32267b7ba8e54a39421d258ba6
ambat25 <25ambat25@gmail.com>
2018-10-30 11:34:44 +0100

    updated syncDb method to return a promise that is awaited in getAllUsers

--- src/lib/userServices.js
+++ src/lib/userServices.js
@@ -5,14 +5,20 @@ import remoteDB from './remoteDB'
 const db = new PouchDB('github-profiles')
 let remoteDb = new PouchDB(remoteDB)
 
-export const fetchUser = async(userID) => {
+export const fetchUser = async (userID) => {
     try {
         const req = await fetch(`https://api.github.com/users/${userID}`)
         const user = await req.json()
         if (!user.login) {
             throw new Error()
         }
-        const profile = { _id: '' + user.id, avatar_url: user.avatar_url, name: user.name, html_url: user.html_url, login: user.login }
+        const profile = {
+            _id: '' + user.id,
+            avatar_url: user.avatar_url,
+            name: user.name,
+            html_url: user.html_url,
+            login: user.login
+        }
         await db.put(profile)
         return profile
     } catch (err) {
@@ -21,18 +27,18 @@ export const fetchUser = async(userID) => {
 }
 
 const syncDb = () => {
-    db.sync(remoteDb).on('complete', function() {
+    return db.sync(remoteDb).on('complete', function () {
         console.log("Sync success")
-    }).on('error', function(err) {
+    }).on('error', function (err) {
         console.log("Error")
     });
 }
 
 export const getAllUsers = async() => {
     try {
+        await syncDb()
         const req = await db.allDocs({ include_docs: true })
         const users = req.rows.map(row => row.doc);
-        syncDb()
         return users
     } catch (err) {
         return {}
